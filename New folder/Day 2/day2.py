5c0fe3b071c66cba95621754c9df5f8451bd5c8b
vaidikmundra <vaidikmundra@gmail.com>
2024-06-28 14:18:41 +0530

    hi

---
+++ New folder/Day 2/day2.py
@@ -0,0 +1,97 @@
+# input function 
+
+# a=input("enter your value ")
+# print(a)
+# print(type(a))
+
+# for num input
+# b=int(input("eneter your valuse"))
+# print(b)
+
+
+# eval function use hota hai koi bhi value print karane ke liye
+# ye tuple , list,float,num, le sakte hai or string ko ''  is ke andar likhna hoga 
+
+# c=eval(input("eneter yor valuse /"))
+# print(c)
+
+# airthmetic operator 
+# assignmentoperator 
+
+# x=10
+# y=20
+
+# x=x+3
+# print(x)
+
+# x+=10
+# print(x)
+
+
+# u=10
+# u-=5
+# print(u)
+
+
+
+# u/=3
+# print(u)
+
+# u*=4
+# print(u)
+
+# u%=3
+# print(u)
+
+
+
+# relatinal operator 
+
+# x=4
+# y=10
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+# print(x!=y)
+
+
+
+# # logicall operator
+
+# a=4
+# b=6
+
+# print(a>b and a!=b)
+# print(a<=b and a)
+# print(1 and a)
+
+
+# print(a>=b or a<=b )
+
+# p=20
+# b=not p
+# print(b)
+
+
+# pp=False
+# bb=not pp
+# print(bb)
+
+# urinary operator single operation performhoga
+# k=10
+# k=(-k)
+# print(k)
+
+
+# p=-100
+# ll=(+p)
+# print(ll)
+
+# bitwise and 
+
+print(10 & 15)
+print (10 | 15)
+
+print(10>>2)
+print(10<<2)
